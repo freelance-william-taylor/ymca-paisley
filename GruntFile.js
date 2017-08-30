9522d93aa807c386a2cdac5c7d06fc94f3c933c6
william-taylor <wi11berto@yahoo.co.uk>
2017-08-29 20:58:36 -0400

    playing around with styles

--- GruntFile.js
+++ GruntFile.js
@@ -37,8 +37,8 @@ module.exports = function (grunt) {
             target: {
                 files: {
                     'build/styles.min.css': [
-                        'node_modules/font-awesome/css/font-awesome.css',
-                        'node_modules/bulma/css/bulma.css',
+                        //'node_modules/font-awesome/css/font-awesome.css',
+                        //'node_modules/bulma/css/bulma.css',
                         'css/*.css'
                     ]
                 }
@@ -46,15 +46,6 @@ module.exports = function (grunt) {
         },
 
         connect: {
-            options: {
-                middleware: function ( connect, options, middlewares ) {
-                    var rules = [
-                        "!\\.html|\\.js|\\.css|\\.svg|\\.jp(e?)g|\\.png|\\.gif$ /index.html"
-                    ];
-                    middlewares.unshift( rewrite( rules ) );
-                    return middlewares;
-                },
-            },
             server: {
                 options: {
                     port: 9000,
